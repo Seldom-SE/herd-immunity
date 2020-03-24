448009127be43be40596042a16a00c359500e331
Seldom <ssaguUUUU@gmail.com>
2020-03-23 22:11:07 -0700

    Player Movement

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(horizontalInput) > 0.001f)
+        {
+            transform.position += transform.right * Mathf.Sign(horizontalInput) * speed;
+        }
+        if (Mathf.Abs(verticalInput) > 0.001f)
+        {
+            transform.position += transform.forward * Mathf.Sign(verticalInput) * speed;
+        }
+    }
+}
