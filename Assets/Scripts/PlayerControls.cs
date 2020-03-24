f6aa36efdca5715519d33da78deafce91d3d659d
Seldom <ssaguUUUU@gmail.com>
2020-03-23 22:31:31 -0700

    Camera Controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,24 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-
     public float speed;
+    public float mouseSensitivity;
 
-    void FixedUpdate()
+    private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
         if (Mathf.Abs(horizontalInput) > 0.001f)
         {
-            transform.position += transform.right * Mathf.Sign(horizontalInput) * speed;
+            transform.position += transform.right * Mathf.Sign(horizontalInput) * speed * Time.fixedDeltaTime;
         }
         if (Mathf.Abs(verticalInput) > 0.001f)
         {
-            transform.position += transform.forward * Mathf.Sign(verticalInput) * speed;
+            transform.position += transform.forward * Mathf.Sign(verticalInput) * speed * Time.fixedDeltaTime;
+        }
+
+        // Camera Movement
+
+        float mouseDX = Input.GetAxis("Mouse X");
+
+        if (Mathf.Abs(mouseDX) > 0.001f)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + mouseDX * mouseSensitivity * Time.fixedDeltaTime, transform.eulerAngles.z);
         }
     }
 }
