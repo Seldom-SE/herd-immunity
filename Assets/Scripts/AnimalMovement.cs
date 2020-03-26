d609506159a1b92a0ced559f18564936eb297d23
Seldom <ssaguUUUU@gmail.com>
2020-03-25 23:42:39 -0700

    Animals and Fleeing

---
+++ Assets/Scripts/AnimalMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    public List<Transform> terrors;
+
+    public float terrorDetectionRadius;
+    public float fleeDistance;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        foreach (Transform terror in terrors)
+        {
+            Vector3 displacement = transform.position - terror.position;
+            if (displacement.magnitude < terrorDetectionRadius)
+            {
+                agent.destination = transform.position + displacement.normalized * fleeDistance;
+            }
+        }
+        
+        animator.SetBool("moving", agent.remainingDistance > 0.1);
+    }
+}
