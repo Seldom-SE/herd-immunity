e7abe06275bd7658babded11dfa793db115e8eac
jefranco <55300629+jefranco@users.noreply.github.com>
2020-03-29 23:51:29 -0700

    Added storm and pen signs

---
+++ Assets/Scripts/Darkening.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkening : MonoBehaviour
+{
+     public float maxRange = 11;
+     public float minRange = 6;
+     public float dimmingSlowness = 0.5f;
+
+     public float timer;
+ 
+     private Light lightSource;
+ 
+     public void Start()
+     {
+         lightSource = GetComponent<Light>();
+     }
+ 
+     public void Update()
+     {
+         timer-=Time.deltaTime;
+         if (timer < 0){
+         lightSource.intensity = Mathf.Lerp(maxRange, minRange, Time.time/dimmingSlowness);
+         }
+     }
+ }
---
+++ Assets/Scripts/SpeedAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedAnimal : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speedForAnimal;
+    void Start()
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().speed = speedForAnimal;
+    }
+
+}
