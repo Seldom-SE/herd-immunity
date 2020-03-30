b07d0da67a445dc28a442dc71fd73b723de0fcd8
jefranco <55300629+jefranco@users.noreply.github.com>
2020-03-30 00:12:52 -0700

    very minor tweaks to sheep sign

--- Assets/Scripts/Darkening.cs
+++ Assets/Scripts/Darkening.cs
@@ -7,8 +7,6 @@ public class Darkening : MonoBehaviour
      public float maxRange = 11;
      public float minRange = 6;
      public float dimmingSlowness = 0.5f;
-
-     public float timer;
  
      private Light lightSource;
  
@@ -19,9 +17,6 @@ public class Darkening : MonoBehaviour
  
      public void Update()
      {
-         timer-=Time.deltaTime;
-         if (timer < 0){
          lightSource.intensity = Mathf.Lerp(maxRange, minRange, Time.time/dimmingSlowness);
-         }
      }
  }
