2931da3429273ee6dce73b90e3cacc8ad48898a2
Seldom <ssaguUUUU@gmail.com>
2020-03-30 20:33:38 -0700

    Tutorial Text

---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+    public float disappearTime;
+    
+    void Update()
+    {
+        if (disappearTime < 0f)
+        {
+            gameObject.SetActive(false);
+        }
+
+        disappearTime -= Time.deltaTime;
+    }
+}
